4e99be74b287dc77b4906ff0328cfcefc9996d7c
Unversedsix0 <gaba.olipires@gmail.com>
2022-08-15 15:56:28 -0300

    3 files modified, 2 added, 3 deleted

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+import './App.css';
+
+function Navbar() {
+  return <h1>BBBBB</h1>;
+}
+
+export default Navbar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Navbar from '../components/Navbar.jsx'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root', 'navbar')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
